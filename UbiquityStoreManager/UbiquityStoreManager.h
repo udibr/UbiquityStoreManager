cbf560e9c61286ccee0479f08845eecd62429aa1
Maarten Billemont <lhunath@lyndir.com>
2013-09-11 16:25:52 -0400

    Relicensed as Apache v2
    [UPDATED]   To avoid issues with the anti-tiviolization of the GPL, relicense as Apache v2.

--- UbiquityStoreManager/UbiquityStoreManager.h
+++ UbiquityStoreManager/UbiquityStoreManager.h
@@ -1,11 +1,22 @@
 /**
- * Copyright Maarten Billemont (http://www.lhunath.com, lhunath@lyndir.com)
+ * Copyright 2012, Maarten Billemont (http://www.lhunath.com, lhunath@lyndir.com)
  *
- * See the enclosed file LICENSE for license information (LASGPLv3).
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  * @author   Maarten Billemont <lhunath@lyndir.com>
- * @license  Lesser-AppStore General Public License
+ * @license  Apache License, Version 2.0
  */
+
 
 //
 //  UbiquityStoreManager.h
